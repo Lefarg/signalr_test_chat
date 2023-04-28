d47b41b2d19fe65fee51c537b12d02cb288cc58e
Deyew Vladimir <deyew.vladimir@yandex.ru>
2023-04-28 15:36:09 +0700

    init

--- lib/features/chat/chat_screen.dart
+++ lib/features/chat/chat_screen.dart
@@ -2,9 +2,14 @@ import 'package:elementary/elementary.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:signalr_test_chat/features/chat/chat_wm.dart';
 
-class ChatScreen extends ElementaryWidget<ChatWM>{
-  const ChatScreen({Key? key, WidgetModelFactory wmFactory = initChatWMFactory}):super(_, key: key,);
+class ChatScreen extends ElementaryWidget<ChatWM> {
+  const ChatScreen({Key? key, WidgetModelFactory wmFactory = initChatWMFactory})
+      : super(
+          wmFactory,
+          key: key,
+        );
   @override
   Widget build(IChatWM wm) {
-  return Container();
+    return Container();
+  }
 }
