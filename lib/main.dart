8f670af9f3468184ab14c82d48cb8ae025c71430
Deyew Vladimir <deyew.vladimir@yandex.ru>
2023-04-28 23:20:10 +0700

    connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:signalr_test_chat/features/chat/chat_screen.dart';
 
 void main() {
+  HttpOverrides.global = HttpOverrideCertificateVerificationInDev();
   runApp(const MyApp());
 }
 
@@ -18,5 +21,14 @@ class MyApp extends StatelessWidget {
       ),
       home: const ChatScreen(),
     );
+  }
+}
+
+class HttpOverrideCertificateVerificationInDev extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
